16e99602544cedadc912073bc172ed35c137007d
Gibson <58445362+GibsonFR@users.noreply.github.com>
2023-10-18 17:21:18 +0200

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,67 @@
+﻿//Using
+global using BepInEx;
+global using BepInEx.IL2CPP;
+global using UnityEngine;
+global using UnityEngine.UI;
+global using UnhollowerRuntimeLib;
+global using HarmonyLib;
+global using System;
+
+namespace Exemple
+{
+    [BepInPlugin("D5C30D3C-EBE2-4A6D-B46C-8A9F216B9924", "SexMod", "1.0.0")]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            ClassInjector.RegisterTypeInIl2Cpp<Exemple>();
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        public class Exemple : MonoBehaviour
+        {
+            bool init;
+            DateTime start = DateTime.Now;
+            void Update()
+            {
+                if (!init && (DateTime.Now - start).TotalSeconds > 2)
+                {
+                    Destroy(GameObject.Find("OnlinePlayer(Clone)/PlayerModel/Pants"));
+                    Destroy(GameObject.Find("OnlinePlayer(Clone)/PlayerModel/Sweater"));
+                    init = true;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GameUI), "Awake")]
+        [HarmonyPostfix]
+        public static void UIAwakePatch(GameUI __instance)
+        {
+            GameObject menuObject = new GameObject();
+            Text text = menuObject.AddComponent<Text>();
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            text.fontSize = 16;
+            text.raycastTarget = false;
+
+            Exemple exemple = menuObject.AddComponent<Exemple>();
+
+            menuObject.transform.SetParent(__instance.transform);
+            menuObject.transform.localPosition = new UnityEngine.Vector3(menuObject.transform.localPosition.x, -menuObject.transform.localPosition.y, menuObject.transform.localPosition.z);
+            RectTransform rt = menuObject.GetComponent<RectTransform>();
+            rt.pivot = new UnityEngine.Vector2(0, 1);
+            rt.sizeDelta = new UnityEngine.Vector2(1920, 1080);
+        }
+        //Anticheat Bypass 
+        [HarmonyPatch(typeof(EffectManager), "Method_Private_Void_GameObject_Boolean_Vector3_Quaternion_0")]
+        [HarmonyPatch(typeof(LobbyManager), "Method_Private_Void_0")]
+        [HarmonyPatch(typeof(MonoBehaviourPublicVesnUnique), "Method_Private_Void_0")]
+        [HarmonyPatch(typeof(LobbySettings), "Method_Public_Void_PDM_2")]
+        [HarmonyPatch(typeof(MonoBehaviourPublicTeplUnique), "Method_Private_Void_PDM_32")]
+        [HarmonyPrefix]
+        public static bool Prefix(System.Reflection.MethodBase __originalMethod)
+        {
+            return false;
+        }
+    }
+}
